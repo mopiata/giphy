d9ffa87a2917d25545aea476f2d861278dc45b3d
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-05-08 11:56:31 +0300

    created some components and styled the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { GifsComponent } from './gifs/gifs.component';
+import { GifDetailsComponent } from './gif-details/gif-details.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    SearchFormComponent,
+    GifsComponent,
+    GifDetailsComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/gif-details/gif-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-details',
+  templateUrl: './gif-details.component.html',
+  styleUrls: ['./gif-details.component.css']
+})
+export class GifDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
